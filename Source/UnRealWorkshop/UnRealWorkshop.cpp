5bc22a44863f9f099680efd5d0034da74490ff17
Gabbersh <gabriel.hamlin@gmail.com>
2024-01-17 14:11:12 +0100

    Startup project

---
+++ Source/UnRealWorkshop/UnRealWorkshop.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnRealWorkshop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnRealWorkshop, "UnRealWorkshop" );
